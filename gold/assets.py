820abdce222390c9b360684861cd7f167a3fef29
macnik <nikos.kyriazis@gmail.com>
2023-09-06 11:02:55 +0300

    updated to include Goldman Sachs Reserves Fund

--- gold/assets.py
+++ gold/assets.py
@@ -6,6 +6,9 @@ import yfinance as yf
 
 def main():
     gold_price, day_difference = get_gold_price()
+    gs_price, gs_day_difference = get_GoldmanSachs_price()
+
+    print(f"Goldman Sachs Liquid Reserves Fund is: ${gs_price:.2f} changing at {gs_day_difference:.2f}%")
     print(f"Gold Price is: ${gold_price:.2f} changing at {day_difference:.2f}%")
 
 def get_gold_price():
@@ -21,6 +24,17 @@ def get_gold_price():
     output = (latest_gold_price - pre_latest_gold_price) / pre_latest_gold_price * 100
 
     return latest_gold_price, output
+
+def get_GoldmanSachs_price():
+    gs_ticker = yf.Ticker("0P00000TMT")
+
+    historical_data = gs_ticker.history(period="3d")
+
+    pre_latest_gs_price = historical_data["Close"].iloc[-2]
+    latest_gs_price = historical_data["Close"].iloc[-1]
+    output = (latest_gs_price - pre_latest_gs_price) / pre_latest_gs_price * 100
+
+    return latest_gs_price, output
 
 if __name__ == "__main__":
     main()
