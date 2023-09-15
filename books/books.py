5bcf77126b2ccb3c57d10cc33a4a93863a32e732
obaino <nikos.kyriazis@gmail.com>
2023-09-15 22:59:38 +0300

    edited books.py

--- books/books.py
+++ books/books.py
@@ -5,8 +5,10 @@ from sys import argv, exit
 from tabulate import tabulate
 import csv
 
+
 def main():
     print(tabulate(get_books(), headers="keys", tablefmt="grid"))
+
 
 def get_books():
     try:
@@ -24,11 +26,14 @@ def get_books():
                     # get the first row of csv as keys
                     key_1, key_2, key_3, key_4, key_5 = reader.fieldnames
                     for row in reader:
-                        books_list.append({key_2: row[key_2], key_3: row[key_3], key_4: row[key_4]})
+                        books_list.append(
+                            {key_2: row[key_2], key_3: row[key_3], key_4: row[key_4]}
+                        )
                     return books_list
 
     except FileNotFoundError:
         exit("File does not exist")
+
 
 if __name__ == "__main__":
     main()
